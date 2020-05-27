7f615d79f686f790a7b2d87c90636d187aef1b46
kwesiandoh <benoo.andoh@gmail.com>
2020-05-27 12:40:45 +0000

    added permissions

--- home/decorators.py
+++ home/decorators.py
@@ -2,17 +2,19 @@ from django.http import HttpResponse
 from django.shortcuts import redirect
 
 
+# stops authenticated user for viewing the login and register pages
 def unauthenticated_user(view_func):
     def wrapper_func(request, *args, **kwargs):
-        # if user is authenticated go to homepage else user must log in
+        # if user is authenticated stay on the dashboard else user must log in
         if request.user.is_authenticated:
+            # dashboard
             return redirect('home')
         else:
             return view_func(request, *args, **kwargs)
-
     return wrapper_func
 
 
+# which user see a page
 def allowed_users(allowed_roles=None):
     if allowed_roles is None:
         allowed_roles = []
@@ -21,16 +23,16 @@ def allowed_users(allowed_roles=None):
         def wrapper_func(request, *args, **kwargs):
 
             group = None
+            # check if user is part of a group
             if request.user.groups.exists():
                 group = request.user.groups.all()[0].name
-
+            # check if group is in the role
             if group in allowed_roles:
                 return view_func(request, *args, **kwargs)
             else:
+                # don not allow the user
                 return HttpResponse("You are not authorized ")
-
         return wrapper_func
-
     return decorator
 
 
