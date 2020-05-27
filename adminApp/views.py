67bee44ed6bc0360588b53c2269a4659bfe6bf09
clement <ckoomson75@gmail.com>
2020-05-27 20:30:37 +0000

    admin dashboard design

--- adminApp/views.py
+++ adminApp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def AdminHomePage(request):
     return render(request,'adminside/admindashboard.html')
+
+def NewSkill(request):
+    return render(request,'adminside/newskill.html')
