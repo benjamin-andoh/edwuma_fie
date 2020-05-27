28f77b82dbaabfaca2c061f3077a34b5c7b28757
AbaidooPrince <abaidooprince@gmail.com>
2020-05-27 16:49:03 +0000

    continuos registration

--- home/static/js/customscripts.js
+++ home/static/js/customscripts.js
@@ -8,18 +8,41 @@ $(document).ready(
 
 
     //the following scripts are for the modals @ homepage
-    function(){
+    function() {
 
-    $("#forgotten").click(function(){
-      $(".modal-footer-forgot").toggle().slideDown();
-      $(".modal-footer").hide();
-        
+        $("#forgotten").click(function() {
+            $(".modal-footer-forgot").toggle().slideDown();
+            $(".modal-footer").hide();
+
+        });
+
+        $("#signUpId").click(function() {
+            $("#loginModal").hide();
+
+        });
+
+        // $("#category").onChange(function() {
+        //     $("#skills").toggle().slideDown();
+        //     // $(".modal-footer").hide();
+
+        // });
+
+        $('#category').on('change', function() {
+            $('#skills').show().slideDown()
+
+        });
+
+        $("li").click(function() {
+            // remove classes from all
+            $("#v-pills-home-tab").removeClass("active");
+            // add class to the one we clicked
+            $("$v-pill-profile").addClass("active");
+        });
+        // $('#default').click(function() {
+        //     $('#skills').hide().slideUp()
+
+        // });
+
+
+
     });
-    
-   $("#signUpId").click(function(){
-    $("#loginModal").hide();
-
-   });
-
-
-  });
