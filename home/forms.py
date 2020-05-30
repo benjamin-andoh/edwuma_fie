d627129179ad120be3a8851c141c712e5c641233
clement <ckoomson75@gmail.com>
2020-05-30 16:10:56 +0000

    verification with email added

--- home/forms.py
+++ home/forms.py
@@ -8,35 +8,36 @@ from django.contrib.auth.models import User
 class CreateUserForm(UserCreationForm):
     username=forms.CharField(
         widget=forms.TextInput(
-            attrs={'class': 'form-control form-control-user', 'placeholder': 'Username'}
+            attrs={'class': 'form-control', 'placeholder': 'Username'}
         )
     )
     first_name = forms.CharField(
         widget=forms.TextInput(
-            attrs={'class': 'form-control form-control-user','placeholder': 'First name'}
+            attrs={'class': 'form-control','placeholder': 'First name','required':'required'}
         )
     )
     last_name = forms.CharField(
         widget=forms.TextInput(
-            attrs={'class': 'form-control form-control-user', 'placeholder': 'Last name'}
+            attrs={'class': 'form-control', 'placeholder': 'Last name' ,'required':'required'}
         )
     )
     password1 = forms.CharField(
         widget=forms.PasswordInput(
-            attrs={'class': 'form-control form-control-user', 'placeholder': 'Password'}
+            attrs={'id':'userPassword','class': 'form-control userPassword validate', 'placeholder': 'Password' ,'required':'required'}
         )
     )
     password2 = forms.CharField(
         widget=forms.PasswordInput(
-            attrs={'class': 'form-control form-control-user', 'placeholder': 'Confirm Password'}
+            attrs={'id':'userConfirmPassword','class': 'form-control userPassword validate', 'placeholder': 'Confirm Password' ,'required':'required'}
         )
     )
     email = forms.CharField(
         widget=forms.EmailInput(
-            attrs={'class': 'form-control form-control-user', 'placeholder': 'Email'}
+            attrs={'class': 'form-control', 'placeholder': 'Email' ,'pattern':'[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$'}
         )
     )
 
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+
--- home/urls.py
+++ home/urls.py
@@ -5,10 +5,11 @@ from . import views
 
 urlpatterns = [
     path('', views.homePage, name='home'),
-    path('register', views.registerPage, name='register'),
+    path('register/', views.registerPage, name='register'),
     path('login', views.loginPage, name='login'),
     path('logout', views.logoutUser, name='logout'),
     path('user', views.userPage, name='user'),
     path('profile',views.accountProfile, name = 'profile'),
+    path('activate/<uidb64>/<token>',views.ActivateAccountView,name='activate'),
 
 ]
